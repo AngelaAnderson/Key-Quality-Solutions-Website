5cb1f118f10721f9d62ddb6477b1b52b9815e133
Angel <aoanderson@sdsu.edu>
2020-04-28 17:11:27 -0700

    updated navigation menu, blog page, and services page

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,28 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("mobileDropdown").classList.toggle("show");
 }
+function navFunction() {
+  document.getElementById("dropdown_about").classList.toggle("show_dropdown");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown_about");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show_dropdown")) {
+        openDropdown.classList.remove("show_dropdown");
+      }
+    }
+  }
+};
+
+function searchFunction() {
+  document.getElementById("dropdown_search").classList.toggle("show_dropdown");
+}
+
 function changeIcon(x) {
   x.classList.toggle("change");
 }
@@ -11,4 +33,21 @@ function changeIcon(x) {
 toggle between hiding and showing the about pages */
 function insideMenuFunction() {
   document.getElementById("mobile_hide").classList.toggle("show_about");
+}
+// function (x) {
+//   x.getElementById("hide_service").classList.toggle("show");
+//   x.getElementById("rightArrow").classList.toggle("rotate");
+// }
+
+function servicesFunction() {
+  var x = document.getElementById("hide_service");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function filterFunction() {
+  document.getElementById("hide_type").classList.toggle("show_dropdown");
 }
